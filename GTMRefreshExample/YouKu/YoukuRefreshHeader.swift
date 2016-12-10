e635c6368f63d5d55f2ab7ffa54d8a86f7677209
GTMYang <289135816@qq.com>
2016-12-10 23:23:52 +0800

    Youku效果demo缺陷

--- GTMRefreshExample/YouKu/YoukuRefreshHeader.swift
+++ GTMRefreshExample/YouKu/YoukuRefreshHeader.swift
@@ -13,7 +13,7 @@ class YoukuRefreshHeader: GTMRefreshHeader, SubGTMRefreshHeaderProtocol {
     let iconImageView = UIImageView()// 这个ImageView用来显示下拉箭头
     let rotatingImageView = UIImageView() //这个ImageView用来播放动图
     let backgroundImageView = UIImageView() //这个ImageView用来显示广告的
-    let frameHeight = UIScreen.main.bounds.size.width * 328.0/571.0
+    let frameHeight: CGFloat = UIScreen.main.bounds.size.width * 328.0/571.0
 
     override init(frame: CGRect) {
         
@@ -35,10 +35,14 @@ class YoukuRefreshHeader: GTMRefreshHeader, SubGTMRefreshHeaderProtocol {
 //    }
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundImageView.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.size.width, height: frameHeight)
+        
+        let x = self.frame.origin.x, y = self.frame.origin.y, w = self.frame.size.width, h = self.frame.size.height
+        
+        backgroundImageView.frame = CGRect(x: x, y: y, width: w, height: frameHeight)
+        backgroundImageView.center = CGPoint(x: w/2, y: h - frameHeight/2)
+        
         iconImageView.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
-       // iconImageView.center = CGPoint(x: self.bounds.width/2.0, y: self.bounds.height/2.0)
-        iconImageView.center = CGPoint(x: self.contentView.bounds.width/2, y: self.contentView.bounds.height/2)
+        iconImageView.center = CGPoint(x: w/2, y: h/2)
         
         rotatingImageView.frame = iconImageView.frame
         rotatingImageView.center = iconImageView.center
@@ -46,9 +50,12 @@ class YoukuRefreshHeader: GTMRefreshHeader, SubGTMRefreshHeaderProtocol {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-//    override func refreshingHoldHeight() -> CGFloat {
-//        return 60.0
+//    override func willRefresHeight() -> CGFloat {
+//        return 60
 //    }
+    override func refreshingHoldHeight() -> CGFloat {
+        return self.backgroundImageView.isHidden ? 60 : frameHeight
+    }
     
     
     func toNormalState() {
